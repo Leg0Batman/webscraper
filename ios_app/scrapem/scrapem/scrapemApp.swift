a1c76973d6497acbdd435ba9a697aaab5ac73a94
Leg0Batman <159012019+Leg0Batman@users.noreply.github.com>
2024-10-24 10:37:24 +0200

    Add iOS app files and assets

---
+++ ios_app/scrapem/scrapem/scrapemApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrapemApp.swift
+//  scrapem
+//
+//  Created by Tim Suskov on 24.10.24.
+//
+
+import SwiftUI
+
+@main
+struct scrapemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios_app/scrapem/scrapemTests/scrapemTests.swift
@@ -0,0 +1,16 @@
+//
+//  scrapemTests.swift
+//  scrapemTests
+//
+//  Created by Tim Suskov on 24.10.24.
+//
+
+import Testing
+
+struct scrapemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
